fec4426f347817fc9b191912df65e5d5081729bc
Dantee-art <santopietrodante@gmail.com>
2026-07-26 14:42:15 -0300

    Update predictor.py

--- scripts/predictor.py
+++ scripts/predictor.py
@@ -2,40 +2,30 @@ from math import pow
 
 HOME_ADV = 0.54
 
-
 def pythag(rs, ra):
     return pow(rs, 1.83) / (pow(rs, 1.83) + pow(ra, 1.83))
 
-
 def log5(a, b):
-    return (a - a * b) / (a + b - 2 * a * b)
-
+    return (a - a*b) / (a + b - 2*a*b)
 
 def elo_prob(a, b):
-    return 1 / (1 + 10 ** ((b - a) / 400))
-
+    return 1 / (1 + 10 ** ((b-a)/400))
 
 def prediction(home, away):
-
     py_home = pythag(home["RS"], home["RA"])
     py_away = pythag(away["RS"], away["RA"])
 
-    l5 = log5(py_home, py_away)
-
+    pyth = log5(py_home, py_away)
     elo = elo_prob(home["elo"], away["elo"])
 
     avg = (home["AVG"] - away["AVG"]) * 0.50
-
-    era = (away["ERA"] - home["ERA"]) * 0.03
-
+    era = (away["ERA"] - home["ERA"]) * 0.08
     ultimos = (home["ultimos10"] - away["ultimos10"]) * 0.02
-
     bullpen = (home["bullpen"] - away["bullpen"]) * 0.01
-
     local = 0.03
 
     prob = (
-        l5 * 0.30 +
+        pyth * 0.45 +
         elo * 0.25 +
         HOME_ADV * 0.10 +
         avg +
@@ -45,10 +35,5 @@ def prediction(home, away):
         local
     )
 
-    if prob > 0.99:
-        prob = 0.99
-
-    if prob < 0.01:
-        prob = 0.01
-
+    prob = max(0.01, min(0.99, prob))
     return round(prob * 100, 1)
